19730729cab589f2db2351b3f0a66b15f76d2b6c
Adam Elders <ramor.1990@gmail.com>
2023-01-28 10:28:01 -0500

    Remove Nebular, we'll try it later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  NbThemeModule,
-  NbLayoutModule,
-  NbSidebarModule,
-  NbSidebarService,
-} from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
@@ -19,12 +12,8 @@ import { HomePageComponent } from './home-page/home-page.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'cosmic' }),
-    NbLayoutModule,
-    NbEvaIconsModule,
-    NbSidebarModule,
   ],
-  providers: [NbSidebarService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
